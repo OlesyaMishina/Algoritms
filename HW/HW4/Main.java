a8b734f3cf2378cba234d79a744e9a5dc07e7636
Olesya Mishina <olesyaavilova33@gmail.com>
2023-08-02 19:23:49 +0300

    HW4

---
+++ HW/HW4/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.add(8);
+        tree.add(1);
+        tree.add(2);
+        tree.add(9);
+        tree.add(5);
+        tree.add(15);
+        tree.add(19);
+        tree.add(17);
+        tree.add(11);
+        tree.add(8);
+        tree.add(12);
+        tree.add(1);
+        tree.add(10);
+        tree.print();
+    }
+}
